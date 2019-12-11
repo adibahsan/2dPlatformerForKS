d73cf64564b3c3e73f8d52804f1d77cf19e7eed4
Adib Ahsan Chowdhury <tjadib@gmail.com>
2019-12-12 00:43:58 +0600

    bullet object added

---
+++ 2DPlatformerController/Assets/bulletScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+	// Start is called before the first frame update
+	[SerializeField]
+	Rigidbody2D rigidbody;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void FixedUpdate()
+	{
+		rigidbody.velocity = new Vector2(3, rigidbody.velocity.y);
+	}
+}
